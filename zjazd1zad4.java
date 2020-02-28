09769ab0f6a24ce4b4055b1b446e5121389f735e
s20682-pj <56191710+s20682-pj@users.noreply.github.com>
2020-02-28 22:51:22 +0100

    Create zjazd1zad4.java

---
+++ zjazd1zad4.java
@@ -0,0 +1,34 @@
+package com.company;
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+            Scanner in = new Scanner(System.in);
+            System.out.println("Podaj wielkość? n = ");
+            int n = in.nextInt();
+            drawPyramid(n);
+    }
+    public static void drawPyramid(int n) {
+        char[] alphabet = "abcdefghijklmnopqrstwuxyz".toCharArray();
+        String newLine = System.getProperty("line.separator");
+        if (n > 0)
+        {
+            System.out.print(alphabet[0] + newLine);
+            for (int i = 1; i < n; i++) {
+                int j = 0;
+                while (j >= 0 && j < i){
+                    System.out.print(alphabet[j]);
+                    j++;
+                }
+                System.out.print(alphabet[j]);
+                j = i - 1;
+                while (j >= 0 && j < n){
+                    System.out.print(alphabet[j]);
+                    j--;
+                }
+                System.out.print(newLine);
+            }
+        }
+    }
+}
