1a1933818f39e6361b78b4f14c0ce4b556fd7f91
s20682-pj <56191710+s20682-pj@users.noreply.github.com>
2020-03-15 16:40:31 +0100

    Create ceasar.java

---
+++ zjazd2/ceasar.java
@@ -0,0 +1,23 @@
+package com.company;
+
+public class ceasar {
+    public static char[] hashCesar(String input) {
+        char[] charInput = input.toCharArray();
+        for (int i=0; i < charInput.length; i++){
+            if((int)charInput[i] >= 65 && (int)charInput[i] <= 87 || (int)charInput[i] >= 97 && (int)charInput[i] <= 119)
+            {
+                charInput[i]+=3;
+            }
+            if((int)charInput[i] >= 88 && (int)charInput[i] < 97) {
+                int temp = (int)charInput[i] - 88;
+                charInput[i] = (char)(temp+65);
+            }
+            if((int)charInput[i] >= 120 && (int)charInput[i] < 123) {
+                int temp = (int) charInput[i] - 120;
+                charInput[i] = (char) (temp + 97);
+
+            }
+        }
+        return charInput;
+    }
+}
