e07e9cbd9a1850019b85753d1f148350df95edc9
s20682-pj <56191710+s20682-pj@users.noreply.github.com>
2020-03-15 16:40:12 +0100

    Create zad8.java

---
+++ zjazd2/zad8.java
@@ -0,0 +1,8 @@
+package com.company;
+
+public class Main {
+    public static void main(String[] args) {
+        String test = "Zuzanna Ciborowska";
+        System.out.println(ceasar.hashCesar(test));
+    }
+}
