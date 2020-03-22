cfc17f817d6595a02a4f29e87c8d192f156114bb
s20682-pj <56191710+s20682-pj@users.noreply.github.com>
2020-03-22 18:03:42 +0100

    Create Main.java

---
+++ zjazd3/zad12/Main.java
@@ -0,0 +1,17 @@
+/*InvoiceItem
+Zuzanna Ciborowska*/
+
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+       InvoiceItem invoiceItem1 = new InvoiceItem("1","pen",3,4.20);
+       System.out.println(invoiceItem1.toString());
+       invoiceItem1.setQty(4);
+       System.out.println("New qty: " + invoiceItem1.getQty());
+       invoiceItem1.setUnitPrice(5.10);
+       System.out.println("New unitPrice: " + invoiceItem1.getUnitPrice());
+       System.out.println("Total price: " + invoiceItem1.getTotal());
+    }
+}
