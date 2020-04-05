b7a49deb32d640f6cad63a32c1c5761f6fb54a9b
s20682-pj <56191710+s20682-pj@users.noreply.github.com>
2020-04-05 17:50:39 +0200

    Create Point3D.java

---
+++ zjazd4/zad21/Point3D.java
@@ -0,0 +1,39 @@
+/*Zuzanna Ciborowska
+Implement the following UML chart (points)*/
+
+package com.company;
+
+public class Point3D extends Point2D {
+    private float z = 0.0f;
+
+    public Point3D(float x, float y, float z) {
+        super(x, y);
+        this.z = z;
+    }
+
+    public Point3D() {
+    }
+
+    public float getZ() {
+        return z;
+    }
+
+    public void setZ(float z) {
+        this.z = z;
+    }
+
+    public void setXYZ(int x, int y, int z){
+        setX(x);
+        setY(y);
+        this.z = z;
+    }
+
+    public float[] getXYZ(){
+        float[] result = {getX(),getY(),z};
+        return result;
+    }
+
+    public String toString(){
+        return "(" + getX() + "," + getY() + "," + this.z + ")";
+    }
+}
