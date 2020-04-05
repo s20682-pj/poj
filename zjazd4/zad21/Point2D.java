96a2215c452acadb76fdb43edb6c3ccbca600fc9
s20682-pj <56191710+s20682-pj@users.noreply.github.com>
2020-04-05 17:50:16 +0200

    Create Point2D.java

---
+++ zjazd4/zad21/Point2D.java
@@ -0,0 +1,47 @@
+/*Zuzanna Ciborowska
+Implement the following UML chart (points)*/
+
+package com.company;
+
+public class Point2D {
+    private float x = 0.0f;
+    private float y = 0.0f;
+
+    public Point2D(float x, float y) {
+        this.x = x;
+        this.y = y;
+    }
+
+    public Point2D() {
+    }
+
+    public float getX() {
+        return x;
+    }
+
+    public void setX(float x) {
+        this.x = x;
+    }
+
+    public float getY() {
+        return y;
+    }
+
+    public void setY(float y) {
+        this.y = y;
+    }
+
+    public void setXY(float x, float y){
+        this.x = x;
+        this.y = y;
+    }
+
+    public float[] getXY(){
+        float[] result = {x,y};
+        return result;
+    }
+
+    public String toString(){
+        return "(" + this.x + "," + this.y + ")";
+    }
+}
