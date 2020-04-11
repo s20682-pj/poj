49b417eb6da711aa145045fdce938625d8051920
s20682-pj <56191710+s20682-pj@users.noreply.github.com>
2020-04-11 18:20:06 +0200

    Create Rectangle.java

---
+++ zjazd4/zad23/Rectangle.java
@@ -0,0 +1,53 @@
+/* Implement the following UML chart
+Shape*/
+
+package com.company;
+
+public class Rectangle extends Shape {
+    private double width = 1.0;
+    private double height = 1.0;
+
+    public Rectangle() {
+    }
+
+    public Rectangle(double width, double height) {
+        this.width = width;
+        this.height = height;
+    }
+
+    public Rectangle(String color, boolean filled, double width, double height) {
+        super(color, filled);
+        this.width = width;
+        this.height = height;
+    }
+
+    public double getWidth() {
+        return width;
+    }
+
+    public void setWidth(double width) {
+        this.width = width;
+    }
+
+    public double getHeight() {
+        return height;
+    }
+
+    public void setHeight(double height) {
+        this.height = height;
+    }
+
+    public double getArea(){
+        double area = width * height;
+        return area;
+    }
+
+    public double getPerimeter(){
+        double perimeter = (2 * width) + (2 * height);
+        return perimeter;
+    }
+
+    public String toString(){
+        return "Shape [color = " + getColor() + ", filled = " + isFilled() + "Rectangle [ width = " + this.width + "height = " + this.height + "]]";
+    }
+}
