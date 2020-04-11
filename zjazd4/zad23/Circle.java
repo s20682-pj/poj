b24b0b12a39f77f9246753bd8ecace711fa6d3c8
s20682-pj <56191710+s20682-pj@users.noreply.github.com>
2020-04-11 18:19:45 +0200

    Create Circle.java

---
+++ zjazd4/zad23/Circle.java
@@ -0,0 +1,42 @@
+/* Implement the following UML chart
+Shape*/
+
+package com.company;
+
+public class Circle extends Shape {
+    private double radius = 1.0;
+
+    public Circle(){
+    }
+
+    public Circle(double radius) {
+        this.radius = radius;
+    }
+
+    public Circle(String color, boolean filled, double radius) {
+        super(color, filled);
+        this.radius = radius;
+    }
+
+    public double getRadius() {
+        return radius;
+    }
+
+    public void setRadius(double radius) {
+        this.radius = radius;
+    }
+
+    public double getArea(){
+        double area = Math.PI * (radius * radius);
+        return area;
+    }
+
+    public double getPerimeter(){
+        double perimeter = 2 * Math.PI * radius;
+        return perimeter;
+    }
+
+    public String toString(){
+        return "Shape [color = " + getColor() + ", filled = " + isFilled() + "Circle [ radius = " + this.radius + "]]";
+    }
+}
