884060ea49c3b9669b9c98adf63db018ff2dc482
s20682-pj <56191710+s20682-pj@users.noreply.github.com>
2020-04-11 18:55:17 +0200

    Create zad28.java

---
+++ zjazd4/zad28.java
@@ -0,0 +1,41 @@
+/*TEMPERATURES
+Solving this puzzle validates that the loop concept is understood and that you can compare a list of values.
+
+This puzzle is also a playground to experiment the concept of lambdas in different programming languages. It's also an opportunity to discover functional programming.
+Zuzanna Ciborowska*/
+
+import java.util.*;
+import java.io.*;
+import java.math.*;
+
+/**
+ * Auto-generated code below aims at helping you parse
+ * the standard input according to the problem statement.
+ **/
+class Solution {
+
+    public static void main(String args[]) {
+        Scanner in = new Scanner(System.in);
+        int N = in.nextInt(); // the number of temperatures to analyse
+        String TEMPS = in.nextLine(); // the N temperatures expressed as integers ranging from -273 to 5526
+        
+        if( N > 0 ) {
+            String list[] = in.nextLine().split(" ");
+       
+            int res = 10000;
+            int temp = 0;
+            for ( int i=0; i<list.length; i++ ) {
+                temp = Integer.parseInt(list[i]);
+                
+                if ( Math.abs(temp) < Math.abs(res)) {
+                    res = temp;
+                }else if ( Math.abs(temp) == Math.abs(res) && temp != res){
+                     res = Math.abs(temp);
+                }
+            }
+            System.out.println(res);
+        } else {
+            System.out.println(0);
+        }        
+    }
+}
