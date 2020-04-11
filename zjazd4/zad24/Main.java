62627bc5ab6eb8f31d89bbfcdc18490169eebe5b
s20682-pj <56191710+s20682-pj@users.noreply.github.com>
2020-04-11 18:33:20 +0200

    Create Main.java

---
+++ zjazd4/zad24/Main.java
@@ -0,0 +1,14 @@
+/*Zuzanna Ciborowska
+Write class to check if a number is Armstrong number or
+not? (input 153 output true, 123 output false)
+*/
+
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+	    System.out.println(Armstrong.isArmstrong(153));
+        System.out.println(Armstrong.isArmstrong(123));
+    }
+}
