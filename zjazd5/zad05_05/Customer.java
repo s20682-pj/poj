5ed71921347e297bb5ff82a8c871fa61dfd9e48f
s20682-pj <56191710+s20682-pj@users.noreply.github.com>
2020-05-09 12:43:55 +0200

    Create Customer.java

---
+++ zjazd5/zad05_05/Customer.java
@@ -0,0 +1,36 @@
+/*Implement the following UML chart (Customer and invoice)
+Zuzanna Ciborowska*/
+
+package com.company;
+
+public class Customer {
+    private int ID;
+    private String name;
+    private int discount;
+
+    public Customer(int ID, String name, int discount) {
+        this.ID = ID;
+        this.name = name;
+        this.discount = discount;
+    }
+
+    public int getID() {
+        return ID;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getDiscount() {
+        return discount;
+    }
+
+    public void setDiscount(int discount) {
+        this.discount = discount;
+    }
+
+    public String toString(){
+        return this.name + "(" + this.ID + ")";
+    }
+}
