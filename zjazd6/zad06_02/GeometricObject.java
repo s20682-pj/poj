7ea96e4068796839fd40b75727c7597365eff183
s20682-pj <56191710+s20682-pj@users.noreply.github.com>
2020-05-19 10:02:30 +0200

    Create GeometricObject.java

---
+++ zjazd6/zad06_02/GeometricObject.java
@@ -0,0 +1,9 @@
+/*Zuzanna Ciborowska
+Implement the following UML chart (Geometric, circle...) */
+
+package com.company;
+
+public interface GeometricObject {
+    public double getPerimeter();
+    public double getArea();
+}
