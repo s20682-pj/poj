a6547af2fc4d4ae14040b7186512819d0687f394
s20682-pj <56191710+s20682-pj@users.noreply.github.com>
2020-05-25 10:24:27 +0200

    Create Turner.java

---
+++ zjazd6/zad06_05/Turner.java
@@ -0,0 +1,19 @@
+/*Zuzanna Ciborowska
+Create an interface named Turner, with a single method named turn().
+Create a class named Leaf that implements turn() to display Changing colors.
+Create a class named Page that implements turn() to display Going to the
+next page. Create a class named Pancake that implements turn() to display
+Flipping.
+Write an application named DemoTurners that creates one object of each of
+these class types and demonstrates the turn() method for each class. Save
+the files as Turner.java, Leaf.java, Page.java, Pancake.java, and
+DemoTurners.java.
+Think of two more objects that use turn(), create classes for them, and then
+add objects to the DemoTurners application, renaming it DemoTurners2.
+java. Save the files, using the names of new objects that use turn().*/
+
+package com.company;
+
+public interface Turner {
+    public void turn();
+}
