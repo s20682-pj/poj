84d4be6c5389c6068ee58f05eb3f46b2933ee30b
s20682-pj <56191710+s20682-pj@users.noreply.github.com>
2020-05-25 10:10:18 +0200

    Create Athlete.java

---
+++ zjazd6/zad06_04/Athlete.java
@@ -0,0 +1,16 @@
+/*Zuzanna Ciborowska
+Create an interface called Runner. The interface has an abstract
+method called run() that displays a message describing the meaning of
+run to the class.
+Create classes called Machine, Athlete, and
+PoliticalCandidate that all implement Runner.*/
+
+package com.company;
+
+public class Athlete implements Runner {
+
+    @Override
+    public void run() {
+        System.out.println("Very fast walking");
+    }
+}
