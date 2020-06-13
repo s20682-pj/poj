ec013f85881ee116c0033afa38481044a038c328
s20682-pj <56191710+s20682-pj@users.noreply.github.com>
2020-06-13 21:29:48 +0200

    Create CacheData.java

---
+++ zjazd7/zad 07_03/CacheData.java
@@ -0,0 +1,12 @@
+/*Zuzanna Ciborowska
+Exercise 07_03*/
+
+package com.company;
+
+public class CacheData {
+    public int ID;
+    public int IntKey;
+    public String StringKey;
+    public String Name;
+    public String Value;
+}
